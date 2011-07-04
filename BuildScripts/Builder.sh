db934ac0860b7f42c90ba62f1c9eb8d2941cbf23
Alex Fairchild <alex.fairchild@gmail.com>
2011-07-03 20:57:15 -0400

    work for ticket #1

--- BuildScripts/Builder.sh
+++ BuildScripts/Builder.sh
@@ -2,17 +2,51 @@
 
 if test $# -lt 1
 then
-        echo "USAGE: <tag to build, HEAD for latest >
+        echo "USAGE: <the tag name, or 'master' for HEAD on master branch >
         Examples: 
 		$0 v01.02
-		$0 HEAD"
+		$0 master"
         exit 1
 fi
 
+currTimestamp=$(date +%Y%m%d%H%M%S);
 buildTag=$1;
-buildDirectory="~/Build";
+buildDirectory="Build/FairchildReservations";
+cd ~/$buildDirectory;
+git fetch --tags;
 
-git tag -l;
-cd $buildDirectory";
-git checkout $buildTag;
+function generateNewTag () {
+	latestTag = $1;
+	#get the position of dot
+	# substring from dot to end
+	# increment by 1
+	# back to string with zero padding
+	return 'v0.03';
+}
+
+
+
+# get list of tags
+latestTag=$(git describe master --tags --abbrev=0);  # gives latest tag on master
+echo "latest tag is $latestTag"
+# if latest tag is not different than latest tag, then nothing to build
+# git diff $latestTag master << empty if no diff
+git diff --exit-code $buildTag $latestTag &> /dev/null ; 
+if [ $? -eq 1 ]
+then 
+	echo "`date` difference found "; 
+	generateNewTag;
+	return_val=$?;
+	echo $return_val;
+fi;
+
+# if different, we generate a new tag. 
+# we tag the last commit, 
+# then we build
+
+#git checkout $buildTag;
+#git pull;
+
+
+
 
---
+++ BuildScripts/build.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+build.py
+
+Created by Alex Fairchild on 2011-07-03.
+Copyright (c) 2011 Fairchild Heavy Industries. All rights reserved.
+
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main(
+		
+	)
+"""
+import sys
+width = 20
+height = 40
+area = width * height
+print area
+beat = "boom"  "Tshack"
+print beat*4
+print "assface" + beat[3:]
