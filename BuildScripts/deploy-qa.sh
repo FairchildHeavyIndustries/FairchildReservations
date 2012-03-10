8fc7c42790e3e759a80231744e2984402c5d84cc
Alex Fairchild <alex.fairchild@gmail.com>
2012-03-10 16:49:33 -0500

    deploy-qa cleans destination

--- BuildScripts/deploy-qa.sh
+++ BuildScripts/deploy-qa.sh
@@ -2,8 +2,11 @@
 
 qa_path="/Users/alex/Sites/qafrs";
 
+echo "wiping out destination directory";
+rm -rf $qa_path/*;
+
 echo "copying ./cake/* to $qa_path";
-cp ./cake/* $qa_path;
+cp -r ./cake/* $qa_path;
 
 echo "seting /tmp permissions to a+rw";
 chmod -R a+rw $qa_path/app/tmp;
