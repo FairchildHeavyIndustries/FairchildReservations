53dca1cd7ce547c4b761ce44372ac2de5aa92a38
Alex Fairchild <alex.fairchild@gmail.com>
2012-03-10 16:32:04 -0500

    deploy-qa copies and changes tmp

--- BuildScripts/deploy-qa.sh
+++ BuildScripts/deploy-qa.sh
@@ -2,4 +2,8 @@
 
 qa_path="/Users/alex/Sites/qafrs";
 
-touch $qa_path/called_from_ant.txt
+echo "copying ./cake/* to $qa_path";
+cp ./cake/* $qa_path;
+
+echo "seting /tmp permissions to a+rw";
+chmod -R a+rw $qa_path/app/tmp;
